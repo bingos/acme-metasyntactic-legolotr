9e5c31ae1c078e664c461e5376e3dae910084bae
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-12-18 13:42:40 +0000

    And one theme in the darkness to bind them

---
+++ lib/Acme/MetaSyntactic/legolotr.pm
@@ -0,0 +1,89 @@
+package Acme::MetaSyntactic::legolotr;
+
+#ABSTRACT: LEGO Lord of the Rings theme
+
+use utf8;
+use strict;
+use warnings;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+
+q[You-Know-Who];
+
+=pod
+
+=encoding utf8
+
+=head1 DESCRIPTION
+
+Characters from the LEGO Lord of the Rings game as gleaned from
+the L<http://www.gamefaqs.com/> walkthroughs.
+
+I<And in the darkness bind them>
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Elendil
+Isildur
+Frodo
+Sam
+Merry
+Pippin
+Aragorn
+Arwen_Evenstar
+Gandalf_the_Grey
+Gandalf_the_White
+Gimli
+Legolas
+Boromir
+Faramir
+Madril
+King_Théoden
+Gollum
+Shagrat
+E�owyn
+Mouth_of_Sauron
+Sauron
+Elrond
+Gil_Galad
+Bilbo_Baggins
+Tom_Bombadil
+Rosie
+Ringwraith
+Gloin
+Moria_Orc
+Uruk_hai
+Lurtz
+Galadriel
+Celeborn
+Gondor_Ranger
+Easterling
+Grishnákh
+Radagast_the_Brown
+Háma
+Gamling
+Grima_Wormtongue
+Haldir
+Lothlórien_Elf
+Berserker
+E�omer
+Gondor_Soldier
+Saruman_the_White
+Mordor_Orc
+King_of_the_Dead
+Soldier_of_the_Dead
+Rohirrim_Soldier
+The_Witch_king
+Denethor
+Gothmog
+Citadel_Guard
+Corsair
+Harardim
+Blacksmith
